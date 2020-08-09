9868b079ac8c653273ab0f9ee19d64910ce799a2
scottvonscott <skeplin17@gmail.com>
2020-08-09 13:22:14 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+const newTask = document.getElementById('create-task-form')
+const taskList = document.getElementById('tasks')
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  newTask.addEventListener('submit', addTask);
+  
 });
+
+const addTask = event => {
+  event.preventDefault();
+
+const newDesc = document.getElementById('new-task-description')
+const task = document.createElement('li')
+task.innerText = newDesc.value;
+taskList.appendChild(task);
+}
